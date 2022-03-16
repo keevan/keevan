d5efc9220e0dbbb78c35e6b80e69ab5c9fa77b35
Kevin Pham <keevan.pham@gmail.com>
2022-03-16 20:06:06 +1100

    Add ability to automatically generate anchor links to different headings
    This allows me and readers to link back to different parts of the article/post that they may want to share. (Usefulness +1)

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,6 +68,7 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          `gatsby-remark-autolink-headers`,
           `gatsby-remark-reading-time`,
           {
             resolve: `gatsby-remark-figure-caption`,
